fb63d927fda6337c04f72d557bbfc8cd82e5c6c0
Jason <32334062+mengdahuang@users.noreply.github.com>
2024-12-12 03:14:46 +0000

    update app

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import sys
 import time
 
 from channel import channel_factory
+from channel.chat_message import ChatMessage
 from common import const
 from config import load_config
 from plugins import *
